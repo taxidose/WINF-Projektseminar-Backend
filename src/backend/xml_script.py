c77949560be1976b7c1e79048fbe86d7f7713228
BelmontR <47500226+BelmontR@users.noreply.github.com>
2022-06-06 17:40:40 +0200

    "One-Filter" Function implemented

--- src/backend/xml_script.py
+++ src/backend/xml_script.py
@@ -7,7 +7,6 @@ ALL_WISO_PUBLICATIONS = r"https://cris.fau.de/ws-cached/public/infoobject/getaut
 ALL_WISO_PROJECTS = r"https://cris.fau.de/ws-cached/public/infoobject/getautorelated/Organisation/105979734/ORGA_2_PROJ_1"
 
 
-
 class XMLProcessing:
     def __init__(self, *, xml_path=None, xml_url=None, xml_url_txt_path=None):
         self.xml_path = xml_path
@@ -41,6 +40,33 @@ class XMLProcessing:
             except Exception:
                 pass
         return list(set(attribute_list))
+
+    #Filtermethode, filtert nach einer Kategorie (f_attr) und einem Wert (f_value). Der Rückgabewert (return_value) ist ein Attribut
+    #der Puplikationen, das zurückgegeben werden soll
+    def filter (tree, f_attr, f_value, return_value):
+        puplis = []
+        #------ Auswahl aller Puplikationen, auf die der Filter passt ------
+        for elem in tree:   #tree = <DataObjects>
+            for e in elem:  #elem = <DataObject>
+                if e.attrib.get("name") == f_attr:  #e = <Attribute>
+                    for var in e:   #var = <Data>
+                        if f_value in str(var.text):
+                            puplis.append(elem) #Fügt alle Puplikationen, auf die der Filter zutrifft, einer Liste hinzu
+                            print(var.text)
+        #------ Auswertung der Filterergebnisse und Rückgabe des gescuhten Attributs ------
+        result = []
+        for elem in puplis:
+            for e in elem:
+                if e.attrib.get("name") == return_value:
+                    for var in e:
+                        result.append(str(var.text))
+        print(len(result))
+        return result
+
+
+
+e = XMLProcessing.get_xml_data(xml_url=ALL_WISO_PUBLICATIONS)
+print(XMLProcessing.filter(e,"srcAuthors", "Sven", "cfTitle"))
 
 
 # XMLProcessor = XMLProcessing()
